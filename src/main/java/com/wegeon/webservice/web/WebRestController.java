cbc937a89740f49b2eb0a794cb585438a968c064
WeGeon <wigon99@gmail.com>
2021-08-22 20:04:45 +0900

    First Spring
    헤으응 시작이당

---
+++ src/main/java/com/wegeon/webservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.wegeon.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world";
+    }
+}
